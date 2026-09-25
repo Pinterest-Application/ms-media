aa9a08af6f729fe082e94873a773e6a82e2ca65b
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2026-09-25 19:30:40 +0400

    feat: initialize project first commit

---
+++ src/test/java/com/example/msmedia/MsMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
